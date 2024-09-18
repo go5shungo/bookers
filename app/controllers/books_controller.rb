ccece4d3b284c94f0707ce6c4def0b293189a2e7
go5shun5 <go5shungo@gmail.com>
2024-09-18 06:53:28 +0000

    bookの新規登録機能の作成

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,7 +1,12 @@
 class BooksController < ApplicationController
   def new
+    @book = Book.new
   end
-
+  def create
+    book = Book.new(book_params)
+    book.save
+    redirect_to '/'
+  end
   def index
   end
 
@@ -9,5 +14,11 @@ class BooksController < ApplicationController
   end
 
   def edit
+  end
+  
+private
+
+  def book_params
+    params.require(:book).permit(:title, :body)
   end
 end
