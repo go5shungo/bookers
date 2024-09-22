3ea425a15e49ca0bca32f16131a2c69f9efa8893
go5shun5 <go5shungo@gmail.com>
2024-09-22 08:02:10 +0000

    [Finish]一覧画面と投稿画面を一つのページにする

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -5,6 +5,7 @@ class BooksController < ApplicationController
   def create
     @book = Book.new(book_params)
     if @book.save
+      flash[:notice] = "Book was successfully created."
       redirect_to book_path(@book.id)
     else
       render :new
@@ -13,6 +14,7 @@ class BooksController < ApplicationController
 
   def index
     @books = Book.all
+    @book = Book.new
   end
 
   def show
